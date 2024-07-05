87034c1c38deaff4679b853f637d3b3180252dbf
Josh Stringer <joshstringer@live.com>
2024-07-04 20:14:04 -0500

    changed shape size a bit, text now prints on top of the shape and changes based on the color user sselected, still needs a bit of adjustment though

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 const fs = require('fs');
 
 const {Circle, Triangle, Square} = require('./library/shapes.js');
-const circle = new Circle(50, 50, 30);
-const triangle = new Triangle(50, 10, 85, 90, 15, 90);
-const square = new Square(30, 30, 50);
+const circle = new Circle(150, 100, 80);
+const triangle = new Triangle(150, 30, 250, 190, 50, 190);
+const square = new Square(75, 50, 150);
 
 inquirer
     .prompt([
@@ -36,26 +36,46 @@ inquirer
         message: 'What color will the shape be?'
     }
 ]).then((data) => {
+
+    const textSize = 50;
+
     if (data.shape === 'Circle') {
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             ${circle.makeCircle()}
+            <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="${data.textColor}" font-size="${textSize}">
+                ${data.text}
+            </text>
             </svg>`;
+
         const svgContent = generateSVG(data);
+
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
+
     } else if (data.shape === 'Triangle') {
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             ${triangle.makeTriangle()}
+            <text x="50%" y="65%" dominant-baseline="middle" text-anchor="middle" fill="${data.textColor}" font-size="${textSize}">
+                ${data.text}
+            </text>
             </svg>`;
+
         const svgContent = generateSVG(data);
+
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
+
     } else if (data.shape === 'Square') {
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             ${square.makeSquare()}
+            <text x="50%" y="63%" dominant-baseline="middle" text-anchor="middle" fill="${data.textColor}" font-size="${textSize}">
+                ${data.text}
+            </text>
             </svg>`;
+
         const svgContent = generateSVG(data);
+
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
     }
 });

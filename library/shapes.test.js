cf3bb600ce78db90b0091d706801d536c7521de0
Josh Stringer <joshstringer@live.com>
2024-07-06 14:51:49 -0500

    got tests to run and they pass, which is great

--- library/shapes.test.js
+++ library/shapes.test.js
@@ -1,23 +1,20 @@
 //imports shapes I want to test
-const {Circle, Triangle, Square} = require('./library/shapes.js');
+const {Circle, Triangle, Square} = require('./shapes.js');
 
 //test for circle
-test('creates circle with user inputted color', () => {
+test('creates circle with makeCircle', () => {
     const circle = new Circle(150, 100, 80);
-    circle.shapeColor('blue');
-    expect(shape.render()).toEqual('<circle cx="150" cy="100" r="80" fill="blue" />');
+    expect(circle.makeCircle()).toEqual('<circle cx="150" cy="100" r="80" />');
 });
 
 //test for triangle
-test('creates triangle with user inputted color', () => {
+test('creates triangle with makeTriangle', () => {
     const triangle = new Triangle(150, 30, 250, 190, 50, 190);
-    triangle.shapeColor('blue');
-    expect(shape.render()).toEqual('<polygon points="150,30 250,190 50,190" fill="blue" />');
+    expect(triangle.makeTriangle()).toEqual('<polygon points="150,30 250,190 50,190" />');
 });
 
 //test for square
-test('creates square with user inputted color', () => {
+test('creates square with makeSquare', () => {
     const square = new Square(75, 50, 150);
-    square.shapeColor('blue');
-    expect(shape.render()).toEqual('<rect x="75" y="50" width="150" height="150" fill="blue" />');
+    expect(square.makeSquare()).toEqual('<rect x="75" y="50" width="150" height="150" />');
 });

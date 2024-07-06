3c02e04870896f2136b290c8f118404c35f51b66
Josh Stringer <joshstringer@live.com>
2024-07-06 11:14:44 -0500

    added a bunch of comments

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+//let's me use inquirer to ask questions
 const inquirer = require('inquirer');
 
 //next two lines add type to inquirer that limits length of response for the input type
 const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
 inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt);
 
+//let's me use fs so that I can write file later
 const fs = require('fs');
 
+//imports the three classes I made in the shapes file and then creates new instances of each class
 const {Circle, Triangle, Square} = require('./library/shapes.js');
 const circle = new Circle(150, 100, 80);
 const triangle = new Triangle(150, 30, 250, 190, 50, 190);
 const square = new Square(75, 50, 150);
 
+//starts inquirer and asks the following questions
 inquirer
     .prompt([
     {
@@ -35,11 +39,12 @@ inquirer
         name: 'shapeColor',
         message: 'What color will the shape be?'
     }
-]).then((data) => {
+]).then((data) => { //takes info and puts it in an object called data
 
-    const textSize = 50;
+    const textSize = 50; //sets text size 50 because the base size was too small for my liking
 
-    if (data.shape === 'Circle') {
+    if (data.shape === 'Circle') { //if the shape is a circle, runs function related to making my circle
+
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <style>
@@ -51,11 +56,12 @@ inquirer
             </text>
             </svg>`;
 
-        const svgContent = generateSVG(data);
+        const svgContent = generateSVG(data); //fills svgContent with the generateSVG function using the data object
 
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
 
-    } else if (data.shape === 'Triangle') {
+    } else if (data.shape === 'Triangle') { //if the shape is a triangle, runs function related to making my triangle
+
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <style>
@@ -67,11 +73,12 @@ inquirer
             </text>
             </svg>`;
 
-        const svgContent = generateSVG(data);
+        const svgContent = generateSVG(data); //fills svgContent with the generateSVG function using the data object
 
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
 
-    } else if (data.shape === 'Square') {
+    } else if (data.shape === 'Square') { //if the shape is a square, runs function related to making my square
+
         const generateSVG = () =>  //function that will eventually create the logo
             `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
             <style>
@@ -83,7 +90,7 @@ inquirer
             </text>
             </svg>`;
 
-        const svgContent = generateSVG(data);
+        const svgContent = generateSVG(data); //fills svgContent with the generateSVG function using the data object
 
         fs.writeFile('./examples/logo.svg', svgContent, (err) => err ? console.log(err) : console.log('Generated logo.svg')); //creates actual svg file in the examples folder
     }
--- library/shapes.js
+++ library/shapes.js
@@ -1,15 +1,15 @@
-class Circle {
+class Circle { //makes a Circle class and sets up it's parameters
     constructor(cx, cy, r) {
         this.cx = cx;
         this.cy = cy;
         this.r = r;
     }
-    makeCircle() {
+    makeCircle() { //function that actual makes the circle for an SVG file
         return `<circle cx="${this.cx}" cy="${this.cy}" r="${this.r}" />`;
     }
 };
 
-class Triangle {
+class Triangle { //makes a Triangle class and sets up it's parameters
     constructor(x1, y1, x2, y2, x3, y3) {
         this.x1 = x1;
         this.y1 = y1;
@@ -18,20 +18,20 @@ class Triangle {
         this.x3 = x3;
         this.y3 = y3;
     }
-    makeTriangle() {
+    makeTriangle() { //function that actual makes the triangle for an SVG file
         return `<polygon points="${this.x1},${this.y1} ${this.x2},${this.y2} ${this.x3},${this.y3}" />`;
     }
 };
 
-class Square {
+class Square { //makes a Square class and sets up it's parameters
     constructor(x, y, sidelength) {
         this.x = x;
         this.y = y;
         this.sidelength = sidelength;
     }
-    makeSquare() {
+    makeSquare() { //function that actual makes the square for an SVG file
         return `<rect x="${this.x}" y="${this.y}" width="${this.sidelength}" height="${this.sidelength}" />`;
     }
 };
 
-module.exports = {Circle, Triangle, Square};
+module.exports = {Circle, Triangle, Square}; //exports the classes for the index file to use later
